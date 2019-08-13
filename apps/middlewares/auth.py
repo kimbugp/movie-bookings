8ffd6902a5009d54a062f175d6eac66259f82dd5
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-08-13 00:23:36 -0700

    c

--- apps/middlewares/auth.py
+++ apps/middlewares/auth.py
@@ -26,9 +26,8 @@ def token_header(f):
                     'message': 'Invalid token'})
             request.user = user
         except Exception as error:
-            raise error
-            # raise ValidationError(message=str(error), status_code=401, payload={
-            #     'message': 'AN error occurred when checking credential'})
+            raise ValidationError(message=str(error), status_code=401, payload={
+                'message': 'AN error occurred when checking credential'})
         return f(*args, **kwargs)
     return decorated
 
