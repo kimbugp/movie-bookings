c316df6e3383e79e54c7b6cad9b80442e7b08486
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-08-14 10:35:45 +0300

    update startup script

--- startup.sh
+++ startup.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-
-echo "<<<<<<<<<<<<<<<<<<<<Load env variable >>>>>>>>>>>>>>>>>>>>>>>>"
-
-source prod.env  
-
 echo "<<<<<<<<<<<<<<<<<<<< Seed database >>>>>>>>>>>>>>>>>>>>>>>>"
 flask seed-database
 sleep 2
 
 echo "<<<<<<<<<<<<<<<<<<<< START API >>>>>>>>>>>>>>>>>>>>>>>>"
 gunicorn run:app -b 0.0.0.0:5000
-
-
-database = 'dfgd'
-
-$database
